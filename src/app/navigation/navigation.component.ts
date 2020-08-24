ee98a993eec72c5a7a043146f9169802f40558d4
Gusvarov <omelchenko.nazarii@gmail.com>
2020-08-24 18:37:55 +0300

    fixes in route and UI

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent implements OnInit {
+export class NavigationComponent implements OnInit, OnDestroy {
+  public name: string;
+  public subscription: Subscription;
 
-  constructor() { }
+  constructor( private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.getUrlName();
+  }
+
+  public getUrlName(): void {
+    this.subscription = this.route.url.subscribe(item => {
+      if (item.length) {
+        this.name = item[0].path;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
