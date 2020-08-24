be3d4a3c80826c5fda4d7da1b1a317ba36dcc35e
Gusvarov <omelchenko.nazarii@gmail.com>
2020-08-24 16:10:43 +0300

    moved navigation to a separated component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconRegistry, MatIconModule } from '@angular/material';
 import { LocationComponent } from './location/location.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    LocationComponent
+    LocationComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
